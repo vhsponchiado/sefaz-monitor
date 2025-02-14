42ccfeebd3133e52e484d9f704b79b4276c97b78
Vinícius H. Sponchiado <vinicius.h.sponchiado@gmail.com>
2025-02-14 15:15:18 -0300

    Register webhook routes in the main application

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import technicalNoteRoutes from "./technicalNoteRoutes";
 import availabilityRoutes from "./availabilityRoutes";
+import webhookRoutes from "./webhookRoutes";
 
 export default async (app: FastifyInstance) => {
   await app.register(technicalNoteRoutes);
   await app.register(availabilityRoutes);
+  await app.register(webhookRoutes);
 };
